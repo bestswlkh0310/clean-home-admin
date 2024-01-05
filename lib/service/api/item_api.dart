39c907d8fb9a47d52d0ab533a55a5d23030e91b1
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-05 20:45:57 +0900

    feat : add to add item in item api

--- lib/service/api/item_api.dart
+++ lib/service/api/item_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:cleanhome/service/api_client.dart';
+import 'package:cleanhome/util/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -8,7 +9,23 @@ import '../../model/item_model.dart';
 
 class ItemApi {
   static Future<List<ItemModel>> getItemAll() async {
-    var response = await ApiClient.client.get(Uri.parse('http://172.30.1.85:3000/item/all'));
+    var response = await ApiClient.client.get(Uri.parse('${Constant.baseUrl}/item/all'));
+    debugPrint(response.body);
+
+    return ItemModel.fromJsonList(json.decode(response.body));
+  }
+
+  static Future<List<ItemModel>> addItem(itemName, cost) async {
+    Map<String, dynamic> body = {
+      'itemName': itemName,
+      'cost': cost
+    };
+    var response = await ApiClient.client.post(
+        Uri.parse('${Constant.baseUrl}/item/add'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: jsonEncode(body));
     return ItemModel.fromJsonList(json.decode(response.body));
   }
 }
