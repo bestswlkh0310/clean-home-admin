acbe91bf7b066db56b734e3aa0e9e2c196af4853
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-05 19:59:20 +0900

    fix : get item all parse json

--- lib/service/api/item_api.dart
+++ lib/service/api/item_api.dart
@@ -1,8 +1,14 @@
+import 'dart:convert';
+
 import 'package:cleanhome/service/api_client.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+import '../../model/item_model.dart';
 
 class ItemApi {
-  static Future<http.Response> getItemAll() {
-    return ApiClient.client.get(Uri.parse('http://172.30.1.85:3000/item'));
+  static Future<List<ItemModel>> getItemAll() async {
+    var response = await ApiClient.client.get(Uri.parse('http://172.30.1.85:3000/item/all'));
+    return ItemModel.fromJsonList(json.decode(response.body));
   }
 }
