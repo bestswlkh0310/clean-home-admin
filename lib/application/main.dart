a5355d48066c81234e2bf1b5379c740724b3df4e
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-01-05 17:36:56 +0900

    feat : handle user uuid

--- lib/application/main.dart
+++ lib/application/main.dart
@@ -2,11 +2,24 @@ import 'package:cleanhome/ui/feature/add_item/add_item_view.dart';
 import 'package:cleanhome/ui/feature/home/home_view.dart';
 import 'package:cleanhome/ui/feature/item/item_view.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 import '../ui/feature/user/user_view.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await handleUUID();
+}
+
+Future<void> handleUUID() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  String uuid = prefs.getString('UUID') ?? '';
+
+  if (uuid.isEmpty) {
+    prefs.setString('UUID', const Uuid().v4());
+  }
 }
 
 class MyApp extends StatelessWidget {
